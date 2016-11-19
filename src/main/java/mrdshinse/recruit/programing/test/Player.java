5397fb66afb2c00823813a121b89da6a54b111ee
mrdShinse <shinse1128@hotmail.com>
2016-11-20 01:29:24 +0900

    create Strategy + DefaultStrategy

--- src/main/java/mrdshinse/recruit/programing/test/Player.java
+++ src/main/java/mrdshinse/recruit/programing/test/Player.java
@@ -1,10 +1,7 @@
 package mrdshinse.recruit.programing.test;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 import lombok.Getter;
 
 /**
@@ -19,10 +16,7 @@ public class Player {
     private final Card card;
     @Getter
     private final List<Card> cardsOnField;
-
-    private static final Comparator<Card> cardComparator = (Card c1, Card c2) -> {
-        return c1.hashCode() - c2.hashCode();
-    };
+    private Strategy strategy;
 
     public Player(String name, Card card) {
         if (name == null || card == null) {
@@ -34,33 +28,10 @@ public class Player {
         this.name = name;
         this.card = card;
         this.cardsOnField = new ArrayList<>();
+        this.strategy = new DefaultStrategy();
     }
 
     public Answer guess(IndianPorkerConfig config, List<AnswerLog> log) {
-        Integer numOfCards = config.getNUM_OF_CARDS();
-        Collections.sort(cardsOnField, cardComparator);
-        Card minNumInField = cardsOnField.get(0);
-        Card maxNumInField = cardsOnField.get(cardsOnField.size() - 1);
-        List<Card> numsExpected = new ArrayList<>();
-        for (int num = 1; num < config.getNUM_OF_CARDS() - 1; num++) {
-            numsExpected.add(new Card(num));
-        }
-        numsExpected.removeAll(cardsOnField);
-        Collections.sort(numsExpected, cardComparator);
-        Card minNumExpected = numsExpected.get(0);
-        Card maxNumExpected = numsExpected.get(numsExpected.size() - 1);
-
-        if (minNumExpected.getNumber() != 1 && !Objects.equals(maxNumExpected.getNumber(), numOfCards)) {
-            return Answer.MID;
-        }
-
-        if (maxNumExpected.getNumber() < minNumInField.getNumber()) {
-            return Answer.MIN;
-        }
-        if (minNumExpected.getNumber() > maxNumInField.getNumber()) {
-            return Answer.MAX;
-        }
-
-        return Answer.CANT_ANSWER;
+        return strategy.guess(this.cardsOnField, config, log);
     }
 }
---
+++ src/main/java/mrdshinse/recruit/programing/test/Strategy.java
@@ -0,0 +1,12 @@
+package mrdshinse.recruit.programing.test;
+
+import java.util.List;
+
+/**
+ *
+ * @author mrdShinse
+ */
+public interface Strategy {
+
+    Answer guess(List<Card> cardsOnField, IndianPorkerConfig config, List<AnswerLog> log);
+}
